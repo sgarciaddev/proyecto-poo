40fdaa16e03180209b6abfdae762e2ecc1104d1b
Sebastian Garcia Delgadillo <sgarciad21@gmail.com>
2021-09-03 08:36:56 -0400

    Actualizacion menor de funcionalidades

--- src/main/java/views/cli/CLI.java
+++ src/main/java/views/cli/CLI.java
@@ -1,8 +1,6 @@
 package views.cli;
 
-import data.AlumnoData;
 import data.CursoData;
-import data.datafile.AlumnoDatafile;
 import data.datafile.CursoDatafile;
 import models.Curso;
 
@@ -13,13 +11,11 @@ import java.util.List;
 
 public class CLI {
     private CursoData cursoData;
-    private AlumnoData alumnoData;
     private List<Curso> cursos;
     private BufferedReader lector;
 
     public CLI() {
         this.cursoData = new CursoDatafile();
-        this.alumnoData = new AlumnoDatafile();
         this.cursos = cursoData.getCursos();
         this.lector = new BufferedReader(new InputStreamReader(System.in));
     }
@@ -54,29 +50,12 @@ public class CLI {
         AlumnoView.mostrarAlumnos(this.cursoData.getCurso(nivel, letra).getAlumnos());
     }
 
-    public void obtenerDatosApoderado() throws IOException {
-        short nivel;
-        int sel;
-        char letra;
-        System.out.print("\nIngrese el nivel (numero de 1 a 12): ");
-        nivel = Short.parseShort(lector.readLine());
-        System.out.print("Ingrese la letra (identifica paralelo): ");
-        letra = lector.readLine().toUpperCase().charAt(0);
-        AlumnoView.mostrarAlumnos(this.cursoData.getCurso(nivel, letra).getAlumnos());
-        System.out.print("Ingrese el numero de alumno: ");
-        sel = Short.parseShort(lector.readLine()) - 1;
-        AlumnoView.mostrarApoderado(this.alumnoData.getAlumno(this.cursoData.getCurso(nivel, letra).getAlumnos().get(sel).getRut()));
-    }
-
     public void menuPrincipal() throws IOException {
-        short opt, nivel;
-        int sel;
-        char letra;
+        short opt;
         do {
             System.out.println("Menu principal");
             System.out.println("  -> (1) Ver cursos");
             System.out.println("  -> (2) Ver alumnos");
-            System.out.println("  -> (3) Obtener datos de apoderado");
             System.out.println("  -> (0) Salir de la aplicacion");
             opt = obtenerOpcionUsuario();
             switch (opt) {
@@ -87,9 +66,6 @@ public class CLI {
                     break;
                 case 2:
                     verAlumnos();
-                    break;
-                case 3:
-                    obtenerDatosApoderado();
                     break;
                 default:
                     opcionErronea();
