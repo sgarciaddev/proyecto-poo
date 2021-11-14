92fcc53901d25afbd9830ba2966ffe01df31f366
Sebastián García <sgarciad21@gmail.com>
2021-11-14 18:59:44 -0300

    Actualizando elementos gráficos

--- src/main/java/aplicacion/controllers/gui/MessageUtilsGUI.java
+++ src/main/java/aplicacion/controllers/gui/MessageUtilsGUI.java
@@ -1,6 +1,9 @@
 package aplicacion.controllers.gui;
 
+import java.awt.Color;
 import javax.swing.*;
+import jiconfont.icons.font_awesome.FontAwesome;
+import jiconfont.swing.IconFontSwing;
 
 /**
  * Clase que controla la generación de mensajes al usuario, en la interfaz gráfica.
@@ -9,6 +12,14 @@ import javax.swing.*;
  * @version 3.0
  */
 public class MessageUtilsGUI {
+    
+    private static final Icon errorIcon = IconFontSwing.buildIcon(FontAwesome.EXCLAMATION_CIRCLE, 40, new Color(255,
+            85, 85));
+    private static final Icon infoIcon = IconFontSwing.buildIcon(FontAwesome.INFO_CIRCLE, 40, new Color(87, 199, 255));
+    private static final Icon questionIcon = IconFontSwing.buildIcon(FontAwesome.QUESTION_CIRCLE, 40, new Color(255, 199, 87));
+    
+    public MessageUtilsGUI() {
+    }
 
     /**
      *
@@ -16,7 +27,7 @@ public class MessageUtilsGUI {
      * @param mensaje
      */
     public static void errorMsg(String mensaje) {
-        JOptionPane.showMessageDialog(null, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showConfirmDialog(null, mensaje, "Error", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE , errorIcon);
     }
 
     /**
@@ -25,16 +36,15 @@ public class MessageUtilsGUI {
      * @param mensaje
      */
     public static void infoMsg(String mensaje) {
-        JOptionPane.showMessageDialog(null, mensaje, "Info", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showConfirmDialog(null, mensaje, "Info", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, infoIcon);
     }
 
     /**
-     *
      * @param mensaje
      * @return
      */
     public static int msjSiNo(String mensaje) {
-        return JOptionPane.showConfirmDialog(null, mensaje, "Confirme", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        return JOptionPane.showConfirmDialog(null, mensaje, "Confirme", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, questionIcon);
     }
 
 }
